5807b361e658020f40dc3e0a6cf7207182e2bc38
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-07-31 19:49:39 +0530

    This Program gives creation of array with values in another method

---
+++ AnotherMethod.java
@@ -0,0 +1,16 @@
+//This Program use to create an array using another method
+//example
+package basics;
+
+public class AnotherMethod 
+{
+	public static void main(String[] args) 
+	{
+		String[] var= {"Swapnil","Akshay","Gaurav","Aftab","Sham"};
+		for(int i=0;i<=var.length-1;i++)
+		{
+			System.out.println(var[i]);
+		}
+	}
+}
+
